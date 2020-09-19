79a2ad624f52bd0330e2f28f42c0b3802acd4fed
James Iorgovan <james.iorgovan@gmail.com>
2020-09-19 01:50:47 -0500

    Fixed temp widget to change numbers (still not correct 1)

--- Assets/WeatherAPIScript.cs
+++ Assets/WeatherAPIScript.cs
@@ -7,16 +7,18 @@ using System;
 
 public class WeatherAPIScript : MonoBehaviour
 {
-    public WeatherInfo Info;
+    public WeatherInfo weatherInfo;
+    public Temperature temperature;
+
 
     public GameObject timeTextObject;
        string url = "http://api.openweathermap.org/data/2.5/weather?lat=41.88&lon=-87.6&APPID=b7bf96a0d52a6ae2fb2d5d5f4159fe43&units=imperial";
    
     void Start()
     {
-    // wait a couple seconds to start and then refresh every 900 seconds
+    // wait a couple seconds to start and then refresh every 30 seconds
 
-       InvokeRepeating("GetDataFromWeb", 2f, 900f);
+       InvokeRepeating("GetDataFromWeb", 2f, 30f);
    }
 
    void GetDataFromWeb()
@@ -25,37 +27,85 @@ public class WeatherAPIScript : MonoBehaviour
        StartCoroutine(GetRequest(url));
    }
 
-    IEnumerator GetRequest(string uri)
-    {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
-        {
-            // Request and wait for the desired page.
-            yield return webRequest.SendWebRequest();
+     private IEnumerator GetRequest(string uri)
+     {
+         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+         {
+             // Request and wait for the desired page.
+             yield return webRequest.SendWebRequest();
 
 
-            if (webRequest.isNetworkError)
-            {
-                Debug.Log(": Error: " + webRequest.error);
-            }
-            else
-            {
-                // print out the weather data to make sure it makes sense
-                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
-            }
-            //Info = JsonUtility.FromJson<WeatherInfo>(url);
-
+             if (webRequest.isNetworkError)
+             {
+                 Debug.Log(": Error: " + webRequest.error);
+             }
+             else
+             {
+                 // print out the weather data to make sure it makes sense
+                 weatherInfo = JsonUtility.FromJson<WeatherInfo>(webRequest.downloadHandler.text);
+             }
+             timeTextObject.GetComponent<TextMeshPro>().text = weatherInfo.temperature.temp.ToString();
         }
-    }
+     }
 }
 
 [System.Serializable]
 public class WeatherInfo
 {
-    public float temp;
-    public float humidity;
-
-    public static WeatherInfo CreateFromJSON(string jsonString){
-        return JsonUtility.FromJson<WeatherInfo>(jsonString);
+    public Coord coord;
+    public Weather[] weather; 
+    public Temperature temperature;
+    public Wind wind;
+    public Location location;
+    public int visibility;
+    public string clouds;
+    public int dt;
+    public int timezone;    
+    public int id;
+    public string name;
+    public int cod;
     }
+
+[System.Serializable]
+public class Coord
+{
+    public float lon;
+    public float lat;
 }
 
+[System.Serializable]
+public class Weather
+{
+    public int id;
+    public string main;
+    public string description;
+    public string icon;
+}
+
+[System.Serializable]
+public class Temperature
+{
+    public float temp;
+    public float feels_like;
+    public float temp_min;
+    public float temp_max;
+    public int pressure;
+    public int humidity;
+}
+
+[System.Serializable]
+public class Wind
+{
+    public float speed;
+    public int deg;
+}
+
+[System.Serializable]
+public class Location
+{
+    public int type;
+    public int id;
+    public string country;
+    public int sunrise;
+    public int sunset;
+}
